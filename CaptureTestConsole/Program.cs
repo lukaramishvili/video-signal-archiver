24540432fb8782ea2210addaf9c1aaaba5aba445
Luka Ramishvili <luka.ramishvili@gmail.com>
2011-11-24 10:17:51 +0400

    added capture library

--- CaptureTestConsole/Program.cs
+++ CaptureTestConsole/Program.cs
@@ -5,14 +5,38 @@ using System.Text;
 using System.Threading;
 using System.IO;
 using System.Globalization;
+using DirectX.Capture;
+using System.Timers;
 
 namespace CaptureTestConsole
 {
+    public class VideoCaptureController
+    {
+        private static Capture capture = null;
+        private static Filters filters = new Filters();
+
+        public bool StartRecording(string sFileName)
+        {
+            if (true == capture.Capturing)
+            {
+                capture.Stop();
+            }
+            capture = new Capture(filters.VideoInputDevices[0], filters.AudioInputDevices[0]);
+            capture.CaptureComplete += new EventHandler(OnCaptureComplete);
+            capture.Filename = sFileName;
+            capture.Start();
+            return true;
+        }
+        private void OnCaptureComplete(object sender, EventArgs e)
+        {
+        }
+    }
     class Program
     {
         public static CultureInfo CultureProvider = CultureInfo.InvariantCulture;
         //
-        public static int unLineCountLastTime = 0;
+        private static int unLineCountLastTime = 0;
+        private static string sGadacemisSaxeliLastTime = "";
         /// <summary>
         /// amocmebs CSV fails, aris tu ara axali gadacemis chanaceri
         /// </summary>
@@ -21,10 +45,11 @@ namespace CaptureTestConsole
                                         , out DateTime dtAxaliGadacemisDackebisDro)
         {
             string[] lsAllLines = File.ReadAllLines("../OAStudioLog20111101.csv");
-            if (lsAllLines.Length > unLineCountLastTime)
+            if (lsAllLines.Length > unLineCountLastTime && sGadacemisSaxeliLastTime != lsAllLines[lsAllLines.Length - 1].Split(',')[4])
             {
                 unLineCountLastTime = lsAllLines.Length;
                 string[] arrBoloStriqonisMonacemebi = lsAllLines[lsAllLines.Length - 1].Split(',');
+                sGadacemisSaxeliLastTime = arrBoloStriqonisMonacemebi[4];
                 //todo guess name from arrBoloStriqonisMonacemebi[4]
                 sShemdegiGadacemisSaxeli = arrBoloStriqonisMonacemebi[4];
                 dtAxaliGadacemisDackebisDro = DateTime.ParseExact(arrBoloStriqonisMonacemebi[5] + " " + arrBoloStriqonisMonacemebi[6]
@@ -41,20 +66,24 @@ namespace CaptureTestConsole
         }
         static void Main(string[] args)
         {
-            while (true)
+            System.Timers.Timer timerResetCheckOrNot = new System.Timers.Timer(3000);
+            timerResetCheckOrNot.Elapsed += delegate(object sender, ElapsedEventArgs e)
             {
                 string sAxaliGadacemisSaxeli;
                 DateTime dtAxaliGadacemisDackebisDro;
                 if (true == nextCSVResult(out sAxaliGadacemisSaxeli, out dtAxaliGadacemisDackebisDro))
                 {
                     Console.WriteLine("new capture {0} at {1}", sAxaliGadacemisSaxeli, dtAxaliGadacemisDackebisDro);
+                    //call capture
                 }
                 else
                 {
                     Console.WriteLine("don't stop capturing");
                 }
-                Thread.Sleep(3000);
-            }
+            };
+            timerResetCheckOrNot.Start();
+            Console.ReadLine();
+            //
         }
     }
 }
