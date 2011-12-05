9804e9d6596834455a934750e63714b3d6a52250
Luka Ramishvili <luka.ramishvili@gmail.com>
2011-12-05 20:04:51 +0400

    add form

---
+++ CaptureTestConsole/RecorderController.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace CaptureTestConsole
+{
+    partial class RecorderController
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStopAndStart = new System.Windows.Forms.Button();
+            this.txtNextGadacemaName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStopAndStart
+            // 
+            this.btnStopAndStart.Location = new System.Drawing.Point(153, 28);
+            this.btnStopAndStart.Name = "btnStopAndStart";
+            this.btnStopAndStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStopAndStart.TabIndex = 0;
+            this.btnStopAndStart.Text = "button1";
+            this.btnStopAndStart.UseVisualStyleBackColor = true;
+            this.btnStopAndStart.Click += new System.EventHandler(this.btnStopAndStart_Click);
+            // 
+            // txtNextGadacemaName
+            // 
+            this.txtNextGadacemaName.Location = new System.Drawing.Point(37, 31);
+            this.txtNextGadacemaName.Name = "txtNextGadacemaName";
+            this.txtNextGadacemaName.Size = new System.Drawing.Size(100, 20);
+            this.txtNextGadacemaName.TabIndex = 1;
+            // 
+            // RecorderController
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 396);
+            this.Controls.Add(this.txtNextGadacemaName);
+            this.Controls.Add(this.btnStopAndStart);
+            this.Name = "RecorderController";
+            this.Text = "RecorderController";
+            this.Load += new System.EventHandler(this.RecorderController_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStopAndStart;
+        private System.Windows.Forms.TextBox txtNextGadacemaName;
+    }
+}
---
+++ CaptureTestConsole/RecorderController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+using System.Globalization;
+using DirectX.Capture;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace CaptureTestConsole
+{
+    public partial class RecorderController : Form
+    {
+        public RecorderController()
+        {
+            InitializeComponent();
+        }
+        //
+        private static string sMainDir = "C:\\";
+        //
+        public static CultureInfo CultureProvider = CultureInfo.InvariantCulture;
+        //
+        private static int unLineCountLastTime = 0;
+        private static string sGadacemisSaxeliLastTime = "";
+        /// <summary>
+        /// amocmebs CSV fails, aris tu ara axali gadacemis chanaceri
+        /// </summary>
+        /// <returns>abrunebs true-s tu axali gadacema daicko, tuarada false-s.</returns>
+        public static bool nextCSVResult(out string sShemdegiGadacemisSaxeli
+                                        , out DateTime dtAxaliGadacemisDackebisDro)
+        {
+            string[] lsAllLines = File.ReadAllLines("../OAStudioLog20111101.csv");
+            if (lsAllLines.Length > unLineCountLastTime && sGadacemisSaxeliLastTime != lsAllLines[lsAllLines.Length - 1].Split(',')[4])
+            {
+                unLineCountLastTime = lsAllLines.Length;
+                string[] arrBoloStriqonisMonacemebi = lsAllLines[lsAllLines.Length - 1].Split(',');
+                sGadacemisSaxeliLastTime = arrBoloStriqonisMonacemebi[4];
+                //todo guess name from arrBoloStriqonisMonacemebi[4]
+                sShemdegiGadacemisSaxeli = arrBoloStriqonisMonacemebi[4].Substring(arrBoloStriqonisMonacemebi[4].LastIndexOf('\\') + 1);
+                dtAxaliGadacemisDackebisDro = DateTime.ParseExact(arrBoloStriqonisMonacemebi[5] + " " + arrBoloStriqonisMonacemebi[6]
+                                                                    , @"dd\/M\/yyyy HH:mm:ss"
+                                                                    , CultureProvider);
+                return true;
+            }
+            else
+            {
+                sShemdegiGadacemisSaxeli = null;
+                dtAxaliGadacemisDackebisDro = DateTime.Now;
+                return false;
+            }
+        }
+
+        public static string add_zeros(string s)
+        {
+            return s.Length > 1 ? s : "0" + s;
+        }
+
+        private void RecorderController_Load(object sender, EventArgs e)
+        {
+            ////VideoCaptureController capturer = new VideoCaptureController();
+            //
+            System.Timers.Timer timerResetCheckOrNot = new System.Timers.Timer(3000);
+            timerResetCheckOrNot.Elapsed += delegate(object senderTimer, ElapsedEventArgs eTimer)
+            {
+                string sAxaliGadacemisSaxeli;
+                DateTime dtAxaliGadacemisDackebisDro;
+                if (true == nextCSVResult(out sAxaliGadacemisSaxeli, out dtAxaliGadacemisDackebisDro))
+                {
+                    //
+                    string sGadacemaName = (0 < sAxaliGadacemisSaxeli.IndexOf("_"))
+                        ? sAxaliGadacemisSaxeli.Substring(0, sAxaliGadacemisSaxeli.IndexOf("_"))
+                        //:"undefined";
+                        : sAxaliGadacemisSaxeli.Substring(0, sAxaliGadacemisSaxeli.LastIndexOf(".")).Replace(" ", "");
+                    //call capture
+                    VideoCaptureController.StartRecording(sPrepareAndReturnFileDestination(sGadacemaName, dtAxaliGadacemisDackebisDro));
+                    //
+                }
+                else
+                {
+                    //Console.WriteLine("don't stop capturing");
+                }
+            };
+            timerResetCheckOrNot.Start();
+            //
+        }
+
+        //creates needed folders and returns formatted file name
+        private string sPrepareAndReturnFileDestination(string sGadacemaName, DateTime dtAxaliGadacemisDackebisDro)
+        {
+            string sDirName = sMainDir + dtAxaliGadacemisDackebisDro.Year + "\\";
+            if (false == Directory.Exists(sDirName))
+            {
+                Directory.CreateDirectory(sDirName);
+            }
+            sDirName += add_zeros(dtAxaliGadacemisDackebisDro.Month.ToString()) + "\\";
+            if (false == Directory.Exists(sDirName))
+            {
+                Directory.CreateDirectory(sDirName);
+            }
+            sDirName += add_zeros(dtAxaliGadacemisDackebisDro.Day.ToString()) + "\\";
+            if (false == Directory.Exists(sDirName))
+            {
+                Directory.CreateDirectory(sDirName);
+            }
+            return sDirName + add_zeros(dtAxaliGadacemisDackebisDro.Hour.ToString())
+                           + "-" + add_zeros(dtAxaliGadacemisDackebisDro.Minute.ToString())
+                           + "-" + sGadacemaName
+                           + ".avi";
+        }
+
+        private void btnStopAndStart_Click(object sender, EventArgs e)
+        {
+            if (0 < txtNextGadacemaName.Text.Length)
+            {
+                VideoCaptureController.StartRecording(sPrepareAndReturnFileDestination(txtNextGadacemaName.Text, DateTime.Now));
+            }
+        }
+    }
+}
