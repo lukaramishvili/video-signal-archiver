09a15ceb1f6c2701fa0b5dc17ba567122ed5c262
Luka Ramishvili <luka.ramishvili@gmail.com>
2011-12-05 20:03:37 +0400

    edited and added form with vs08

--- CaptureTestConsole/Program.cs
+++ CaptureTestConsole/Program.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Globalization;
 using DirectX.Capture;
 using System.Timers;
+using System.Windows.Forms;
 
 namespace CaptureTestConsole
 {
@@ -31,6 +32,7 @@ namespace CaptureTestConsole
             capture.CaptureComplete += new EventHandler(OnCaptureComplete);
             capture.Filename = sFileName;
             capture.Start();
+            Console.WriteLine("new capture {0}", sFileName);
             return true;
         }
         private static void OnCaptureComplete(object sender, EventArgs e)
@@ -39,93 +41,12 @@ namespace CaptureTestConsole
     }
     class Program
     {
-        //
-        private static string sMainDir = "C:\\";
-        //
-        public static CultureInfo CultureProvider = CultureInfo.InvariantCulture;
-        //
-        private static int unLineCountLastTime = 0;
-        private static string sGadacemisSaxeliLastTime = "";
-        /// <summary>
-        /// amocmebs CSV fails, aris tu ara axali gadacemis chanaceri
-        /// </summary>
-        /// <returns>abrunebs true-s tu axali gadacema daicko, tuarada false-s.</returns>
-        public static bool nextCSVResult(out string sShemdegiGadacemisSaxeli
-                                        , out DateTime dtAxaliGadacemisDackebisDro)
-        {
-            string[] lsAllLines = File.ReadAllLines("../OAStudioLog20111101.csv");
-            if (lsAllLines.Length > unLineCountLastTime && sGadacemisSaxeliLastTime != lsAllLines[lsAllLines.Length - 1].Split(',')[4])
-            {
-                unLineCountLastTime = lsAllLines.Length;
-                string[] arrBoloStriqonisMonacemebi = lsAllLines[lsAllLines.Length - 1].Split(',');
-                sGadacemisSaxeliLastTime = arrBoloStriqonisMonacemebi[4];
-                //todo guess name from arrBoloStriqonisMonacemebi[4]
-                sShemdegiGadacemisSaxeli = arrBoloStriqonisMonacemebi[4].Substring(arrBoloStriqonisMonacemebi[4].LastIndexOf('\\')+1);
-                dtAxaliGadacemisDackebisDro = DateTime.ParseExact(arrBoloStriqonisMonacemebi[5] + " " + arrBoloStriqonisMonacemebi[6]
-                                                                    , @"dd\/M\/yyyy HH:mm:ss"
-                                                                    , CultureProvider);
-                return true;
-            }
-            else
-            {
-                sShemdegiGadacemisSaxeli = null;
-                dtAxaliGadacemisDackebisDro = DateTime.Now;
-                return false;
-            }
-        }
+        [STAThread]
         static void Main(string[] args)
         {
-            ////VideoCaptureController capturer = new VideoCaptureController();
-            //
-            System.Timers.Timer timerResetCheckOrNot = new System.Timers.Timer(3000);
-            timerResetCheckOrNot.Elapsed += delegate(object sender, ElapsedEventArgs e)
-            {
-                string sAxaliGadacemisSaxeli;
-                DateTime dtAxaliGadacemisDackebisDro;
-                if (true == nextCSVResult(out sAxaliGadacemisSaxeli, out dtAxaliGadacemisDackebisDro))
-                {
-                    Console.WriteLine("new capture {0} at {1}", sAxaliGadacemisSaxeli, dtAxaliGadacemisDackebisDro);
-                    //
-                    string sGadacemaName = (0 < sAxaliGadacemisSaxeli.IndexOf("_"))
-                        ? sAxaliGadacemisSaxeli.Substring(0, sAxaliGadacemisSaxeli.IndexOf("_"))
-                        //:"undefined";
-                        : sAxaliGadacemisSaxeli.Substring(0, sAxaliGadacemisSaxeli.LastIndexOf(".")).Replace(" ","");
-                    string sFileName = add_zeros(dtAxaliGadacemisDackebisDro.Hour.ToString())
-                        + "-" + add_zeros(dtAxaliGadacemisDackebisDro.Minute.ToString())
-                        + "-" + sGadacemaName
-                        + ".avi";
-                    string sDirName = sMainDir + dtAxaliGadacemisDackebisDro.Year + "\\";
-                    if (false == Directory.Exists(sDirName))
-                    {
-                        Directory.CreateDirectory(sDirName);
-                    }
-                    sDirName += add_zeros(dtAxaliGadacemisDackebisDro.Month.ToString()) + "\\";
-                    if (false == Directory.Exists(sDirName))
-                    {
-                        Directory.CreateDirectory(sDirName);
-                    }
-                    sDirName += add_zeros(dtAxaliGadacemisDackebisDro.Day.ToString()) + "\\";
-                    if (false == Directory.Exists(sDirName))
-                    {
-                        Directory.CreateDirectory(sDirName);
-                    }
-                    //call capture
-                    VideoCaptureController.StartRecording(sDirName + sFileName);
-                    //
-                }
-                else
-                {
-                    Console.WriteLine("don't stop capturing");
-                }
-            };
-            timerResetCheckOrNot.Start();
-            Console.ReadLine();
-            //
-        }
-
-        public static string add_zeros(string s)
-        {
-            return s.Length > 1 ? s : "0" + s;
+            RecorderController frmRecorderController = new RecorderController();
+            Application.EnableVisualStyles();
+            Application.Run(frmRecorderController);
         }
     }
 }
