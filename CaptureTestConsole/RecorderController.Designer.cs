889e659fc8b1ea633cfa089c3c3e10b9ba3c97fe
Luka Ramishvili <luka.ramishvili@gmail.com>
2011-12-07 09:08:43 +0400

    refined interface

--- CaptureTestConsole/RecorderController.Designer.cs
+++ CaptureTestConsole/RecorderController.Designer.cs
@@ -28,9 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnStopAndStart = new System.Windows.Forms.Button();
-            this.txtNextGadacemaName = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.tabContainer = new System.Windows.Forms.TabControl();
             this.tab_recorder = new System.Windows.Forms.TabPage();
@@ -39,37 +36,18 @@
             this.colStartTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colEndTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colGadacemisSaxeli = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.gpboxAxaliGadacema = new System.Windows.Forms.GroupBox();
+            this.lblAxaliGadacema = new System.Windows.Forms.Label();
+            this.txtNextGadacemaName = new System.Windows.Forms.TextBox();
+            this.btnStopAndStart = new System.Windows.Forms.Button();
+            this.green_light = new System.Windows.Forms.PictureBox();
             this.tabContainer.SuspendLayout();
             this.tab_recorder.SuspendLayout();
             this.tab_gadacemebi.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.gpboxAxaliGadacema.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.green_light)).BeginInit();
             this.SuspendLayout();
-            // 
-            // btnStopAndStart
-            // 
-            this.btnStopAndStart.Location = new System.Drawing.Point(251, 19);
-            this.btnStopAndStart.Name = "btnStopAndStart";
-            this.btnStopAndStart.Size = new System.Drawing.Size(75, 23);
-            this.btnStopAndStart.TabIndex = 0;
-            this.btnStopAndStart.Text = "button1";
-            this.btnStopAndStart.UseVisualStyleBackColor = true;
-            this.btnStopAndStart.Click += new System.EventHandler(this.btnStopAndStart_Click);
-            // 
-            // txtNextGadacemaName
-            // 
-            this.txtNextGadacemaName.Location = new System.Drawing.Point(6, 106);
-            this.txtNextGadacemaName.Name = "txtNextGadacemaName";
-            this.txtNextGadacemaName.Size = new System.Drawing.Size(100, 20);
-            this.txtNextGadacemaName.TabIndex = 1;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(6, 6);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(69, 67);
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
             // 
             // dateTimePicker1
             // 
@@ -91,9 +69,8 @@
             // 
             // tab_recorder
             // 
-            this.tab_recorder.Controls.Add(this.pictureBox1);
-            this.tab_recorder.Controls.Add(this.btnStopAndStart);
-            this.tab_recorder.Controls.Add(this.txtNextGadacemaName);
+            this.tab_recorder.Controls.Add(this.gpboxAxaliGadacema);
+            this.tab_recorder.Controls.Add(this.green_light);
             this.tab_recorder.Location = new System.Drawing.Point(4, 22);
             this.tab_recorder.Name = "tab_recorder";
             this.tab_recorder.Padding = new System.Windows.Forms.Padding(3);
@@ -145,6 +122,55 @@
             this.colGadacemisSaxeli.Name = "colGadacemisSaxeli";
             this.colGadacemisSaxeli.Width = 250;
             // 
+            // gpboxAxaliGadacema
+            // 
+            this.gpboxAxaliGadacema.Controls.Add(this.lblAxaliGadacema);
+            this.gpboxAxaliGadacema.Controls.Add(this.btnStopAndStart);
+            this.gpboxAxaliGadacema.Controls.Add(this.txtNextGadacemaName);
+            this.gpboxAxaliGadacema.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gpboxAxaliGadacema.Location = new System.Drawing.Point(184, 98);
+            this.gpboxAxaliGadacema.Name = "gpboxAxaliGadacema";
+            this.gpboxAxaliGadacema.Size = new System.Drawing.Size(292, 149);
+            this.gpboxAxaliGadacema.TabIndex = 4;
+            this.gpboxAxaliGadacema.TabStop = false;
+            this.gpboxAxaliGadacema.Text = "შემდეგი გადაცემის ჩაწერის დაწყება";
+            // 
+            // lblAxaliGadacema
+            // 
+            this.lblAxaliGadacema.AutoSize = true;
+            this.lblAxaliGadacema.Location = new System.Drawing.Point(6, 44);
+            this.lblAxaliGadacema.Name = "lblAxaliGadacema";
+            this.lblAxaliGadacema.Size = new System.Drawing.Size(166, 18);
+            this.lblAxaliGadacema.TabIndex = 6;
+            this.lblAxaliGadacema.Text = "ახალი გადაცემის სახელი";
+            // 
+            // txtNextGadacemaName
+            // 
+            this.txtNextGadacemaName.Location = new System.Drawing.Point(9, 75);
+            this.txtNextGadacemaName.Name = "txtNextGadacemaName";
+            this.txtNextGadacemaName.Size = new System.Drawing.Size(153, 24);
+            this.txtNextGadacemaName.TabIndex = 5;
+            // 
+            // btnStopAndStart
+            // 
+            this.btnStopAndStart.FlatAppearance.BorderSize = 0;
+            this.btnStopAndStart.Image = global::CaptureTestConsole.Properties.Resources.stopplay_mini;
+            this.btnStopAndStart.Location = new System.Drawing.Point(203, 44);
+            this.btnStopAndStart.Name = "btnStopAndStart";
+            this.btnStopAndStart.Size = new System.Drawing.Size(66, 56);
+            this.btnStopAndStart.TabIndex = 4;
+            this.btnStopAndStart.UseVisualStyleBackColor = true;
+            this.btnStopAndStart.Click += new System.EventHandler(this.btnStopAndStart_Click);
+            // 
+            // green_light
+            // 
+            this.green_light.Image = global::CaptureTestConsole.Properties.Resources.green_light_still;
+            this.green_light.Location = new System.Drawing.Point(47, 132);
+            this.green_light.Name = "green_light";
+            this.green_light.Size = new System.Drawing.Size(88, 91);
+            this.green_light.TabIndex = 2;
+            this.green_light.TabStop = false;
+            // 
             // RecorderController
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -154,21 +180,20 @@
             this.Name = "RecorderController";
             this.Text = "RecorderController";
             this.Load += new System.EventHandler(this.RecorderController_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.tabContainer.ResumeLayout(false);
             this.tab_recorder.ResumeLayout(false);
-            this.tab_recorder.PerformLayout();
             this.tab_gadacemebi.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.gpboxAxaliGadacema.ResumeLayout(false);
+            this.gpboxAxaliGadacema.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.green_light)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnStopAndStart;
-        private System.Windows.Forms.TextBox txtNextGadacemaName;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox green_light;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.TabControl tabContainer;
         private System.Windows.Forms.TabPage tab_recorder;
@@ -177,5 +202,9 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn colStartTime;
         private System.Windows.Forms.DataGridViewTextBoxColumn colEndTime;
         private System.Windows.Forms.DataGridViewTextBoxColumn colGadacemisSaxeli;
+        private System.Windows.Forms.GroupBox gpboxAxaliGadacema;
+        private System.Windows.Forms.Label lblAxaliGadacema;
+        private System.Windows.Forms.Button btnStopAndStart;
+        private System.Windows.Forms.TextBox txtNextGadacemaName;
     }
 }
